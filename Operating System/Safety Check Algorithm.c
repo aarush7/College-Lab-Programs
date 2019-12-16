1f8cce5a6360658f36083730da4019362c00e51b
Aarush Gupta <50203165+aarush7@users.noreply.github.com>
2019-12-16 14:37:20 +0530

    Update Safety Check Algorithm.c

--- Operating System/Safety Check Algorithm.c
+++ Operating System/Safety Check Algorithm.c
@@ -1,52 +1,101 @@
 #include <stdio.h>
- 
-char string[50], n;
-void subset(int, int, int);
- 
 int main()
 {
-    int i, len;
- 
-    printf("Enter the len of main set : ");
-    scanf("%d", &len);
-    printf("Enter the elements of main set : ");
-    scanf("%s", string);
-    n = len;
-    printf("The subsets are { Φ ,");
-    for (i = 1;i <= n;i++)
-        subset(0, 0, i);
-    printf("}\n\n\n");
-}
- 
+int Max[10][10], need[10][10], alloc[10][10], avail[10], completed[10], safeSequence[10];
+int p, r, i, j, process, count;
+count = 0;
 
- 
-void subset(int start, int index, int num_sub)
+printf("Enter the no of processes : ");
+scanf("%d", &p);
+
+for(i = 0; i< p; i++)
+    completed[i] = 0;
+
+printf("\n\nEnter the no of resources : ");
+scanf("%d", &r);
+
+printf("\n\nEnter the Max Matrix for each process : ");
+for(i = 0; i < p; i++)
 {
-    int i, j;
-    if (index - start + 1  ==  num_sub)
+    printf("\nFor process %d : ", i + 1);
+    for(j = 0; j < r; j++)
+        scanf("%d", &Max[i][j]);
+}
+
+printf("\n\nEnter the allocation for each process : ");
+for(i = 0; i < p; i++)
+{
+    printf("\nFor process %d : ",i + 1);
+    for(j = 0; j < r; j++)
+        scanf("%d", &alloc[i][j]);
+}
+
+printf("\n\nEnter the Available Resources : ");
+for(i = 0; i < r; i++)
+        scanf("%d", &avail[i]);
+
+
+    for(i = 0; i < p; i++)
+        for(j = 0; j < r; j++)
+            need[i][j] = Max[i][j] - alloc[i][j];
+        
+do
+{
+    printf("\n Max matrix:\tAllocation matrix:\n");
+    for(i = 0; i < p; i++)
     {
-        if (num_sub  ==  1)
+        for( j = 0; j < r; j++)
+            printf("%d  ", Max[i][j]);
+        printf("\t\t");
+        for( j = 0; j < r; j++)
+            printf("%d  ", alloc[i][j]);
+        printf("\n");
+    }
+
+    process = -1;
+
+    for(i = 0; i < p; i++)
+    {
+        if(completed[i] == 0)//if not completed
         {
-            for (i = 0;i < n;i++)
-            {printf("{%c}", string[i]);
-                if((i+1)!=n)
-                    printf(",");}
-        }
-        else
-        {
-            for (j = index;j < n;j++)
-              
-            {printf(", {");
-                for (i = start;i < index;i++)
-                    printf("%c,", string[i]);
-                printf("%c} ", string[j]);
+            process = i ;
+            for(j = 0; j < r; j++)
+            {
+                if(avail[j] < need[i][j])
+                {
+                    process = -1;
+                    break;
+                }
             }
-            if (start != n - num_sub)
-                subset(start + 1, start + 1, num_sub);
+        }
+        if(process != -1)
+            break;
+    }
+
+    if(process != -1)
+    {
+        printf("\nProcess %d runs to completion!", process + 1);
+        safeSequence[count] = process + 1;
+        count++;
+        for(j = 0; j < r; j++)
+        {
+            avail[j] += alloc[process][j];
+            alloc[process][j] = 0;
+            Max[process][j] = 0;
+            completed[process] = 1;
         }
     }
-    else
-    {
-        subset(start, index + 1, num_sub);
-    }
+}while(count != p && process != -1);
+
+if(count == p)
+{
+    printf("\nThe system is in a safe state!!\n");
+    printf("Safe Sequence : < ");
+    for( i = 0; i < p; i++)
+            printf("%d  ", safeSequence[i]);
+    printf(">\n");
+}
+else
+    printf("\nThe system is in an unsafe state!!");
+
 }
